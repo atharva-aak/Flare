ae7cc22840fdc5d757ff0839ec21b336f5b94b01
atharva-aak <atharvakalbande5@gmail.com>
2025-02-03 22:43:07 +0530

    Added the (plain) signup page

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,13 @@
+import * as z from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {message:'Too Short'}),
+
+    username: z.string().min(2,{message:'Invalid email'}),
+
+    email: z.string().email(),
+
+    password: z.string().min(8,{message: 'Password Must be atleast 8 characters'}),
+
+
+})
