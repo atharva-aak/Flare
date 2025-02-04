1cdbf4c3df60d7f665b9c169e50867adcab7740c
atharva-aak <atharvakalbande5@gmail.com>
2025-02-04 17:11:42 +0530

    Established Routing Successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Authlayout from './_auth/AuthLayout';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import RootLayout from './_root/RootLayout';
 import './globals.css';
 import {Routes, Route} from 'react-router-dom';
@@ -25,6 +25,14 @@ const App = () => {
 
             <Route element={<RootLayout />}>
                 <Route index element={<Home />}/>
+                <Route path="/explore" element={<Explore />}/>
+                <Route path="/saved" element={<Saved />}/>
+                <Route path="/all-users" element={<AllUsers />}/>
+                <Route path="/create-post" element={<CreatePost />}/>
+                <Route path="/update-post/:id" element={<EditPost />}/>
+                <Route path="/posts/:id" element={<PostDetails />}/>
+                <Route path="/profile/:id/*" element={<Profile />}/>
+                <Route path="/update-profile/:id" element={<UpdateProfile />}/>
 
             </Route>
         </Routes>
